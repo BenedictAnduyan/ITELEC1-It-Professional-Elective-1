c64bdbace407bdcd2e9755e1d8e4176c83741a35
BenedictAnduyan <anduyanbenedict_bsit@plmun.edu.ph>
2025-10-30 16:49:25 +0800

    Update MainActivity.kt

--- FINALS LAB #1 EXERCISE #2/MainActivity.kt
+++ FINALS LAB #1 EXERCISE #2/MainActivity.kt
@@ -1,1 +1,18 @@
+package com.example.mainactivity
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+   
+        val myTextView: TextView = findViewById(R.id.myTextview)
+        val labelText: TextView = findViewById(R.id.labelText)
+        val inputName: EditText = findViewById(R.id.inputName)
+    }
+}
